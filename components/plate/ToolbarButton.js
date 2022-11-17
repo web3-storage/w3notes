9a346d7baf53190060d0ef198160052b7319ba75
Travis Vachon <travis.vachon@gmail.com>
2022-11-16 20:51:48 -0800

    fix: get dark mode looking right

--- components/plate/ToolbarButton.js
+++ components/plate/ToolbarButton.js
@@ -1,6 +1,6 @@
 
 export default function ToolbarButton ({ className = 'w-7 h-7', active, ...props }) {
-  const allClassNames = `${className} ${active ? 'text-blue-700' : 'bg-white'}`
+  const allClassNames = `${className} ${active ? 'text-blue-700' : ''}`
 
   return (
     <button
